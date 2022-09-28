da02d81b4db33f578a999d12fb3a01ba2a9ab840
lisamarimyrene <lisamy@stud.ntnu.no>
2022-09-28 20:33:32 +0200

    portfolio first draft done

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+document.getElementById("li__skills").addEventListener("click", function() {
+    window.scrollTo({
+        top: 790,
+        left: 0,
+        behavior: 'smooth'
+     });
+  });
+
+document.getElementById("li__project").addEventListener("click", function() {
+    window.scrollTo({
+        top: 1580,
+        left: 0,
+        behavior: 'smooth'
+     });
+});
+
+document.getElementById("li__aboutme").addEventListener("click", function() {
+    window.scrollTo({
+        top: 2500,
+        left: 0,
+        behavior: 'smooth'
+     });
+});
+
+
+
+// $("button").click(function() {
+//     $('html,body').animate({
+//         scrollTop: $(".section__skills--desktop").offset().top},
+//         'slow');
+// });
