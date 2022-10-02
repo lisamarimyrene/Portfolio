067d0aa8720888c8a1aa7c4ebea7e4b977051e4a
lisamarimyrene <lisamy@stud.ntnu.no>
2022-10-02 15:18:57 +0200

    fixed navbar scrolling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,34 @@
-document.getElementById("li__skills").addEventListener("click", function() {
-    window.scrollTo({
-        top: 790,
-        left: 0,
-        behavior: 'smooth'
-     });
-  });
+// var element = document.getElementById("li__skills");
+// element.addEventListener("click", function() {
+//     element.scrollIntoView({
+//         behavior: 'smooth',
+//         block: "center"
+//      });
+//   });
 
-document.getElementById("li__project").addEventListener("click", function() {
-    window.scrollTo({
-        top: 1580,
-        left: 0,
-        behavior: 'smooth'
-     });
-});
+// document.getElementById("li__project").addEventListener("click", function() {
+//     window.scrollTo({
+//         top: 1580,
+//         left: 0,
+//         behavior: 'smooth'
+//      });
+// });
 
-document.getElementById("li__aboutme").addEventListener("click", function() {
-    window.scrollTo({
-        top: 2500,
-        left: 0,
-        behavior: 'smooth'
-     });
-});
+// document.getElementById("li__aboutme").addEventListener("click", function() {
+//     window.scrollTo({
+//         top: 2500,
+//         left: 0,
+//         behavior: 'smooth'
+//      });
+// });
 
+
+// let btnSkills = document.querySelector('.li__skills');
+// let sectionSkills = document.querySelector('section__skills--desktop');
+
+// btnSkills.addEventListener('click', function() {
+//     sectionSkills.scrollIntoView(true);
+// }); 
 
 
 // $("button").click(function() {
